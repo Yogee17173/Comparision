582fe3c02d9362428b3086e50cfe7d096e8456b1
YOGEESH K P <yogeesh793@gmail.com>
2025-02-24 13:41:38 +0530

    Add files via upload

---
+++ Comparision.py
@@ -0,0 +1,21 @@
+# Python Comparision Operators
+a=4
+b=5
+# Equal
+print("a==b:",a==b)
+
+# Not Equal
+print("a!=b:",a!=b)
+
+# Greater than
+print("a>b:",a>b)
+
+# Less than
+print("a<b:",a<b)
+
+# Greater than or Equal to
+print("a>=b:",a>=b)
+
+# Less than or Equal to
+print("a<=b:",a<=b)
+
